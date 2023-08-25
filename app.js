8c7b497d7ea8840f3640cab5f028f9683697308d
codedbyemem <eumoabasi@gmail.com>
2023-08-25 17:51:52 +0100

    edit fix

--- app.js
+++ app.js
@@ -60,12 +60,12 @@ function displayInputContents(event) {
   editBtn.classList.add("edit-btn");
 
   //creating a new line of the html page displaying the inputs
-  const newLine = document.createTextNode(
+  let currentLine = document.createTextNode(
     `${formatDate} ${exerciseInputText} ${durationInputH} H ${durationInputM} M ${notes}`
   );
 
   //adding the new line of text and the button in the DOM div
-  wrapper.append(newLine, deleteBtn, editBtn);
+  wrapper.append(currentLine, deleteBtn, editBtn);
 
   //making sure display shows most recent input first
   displayAll.insertBefore(wrapper, displayAll.childNodes[0]);
@@ -83,8 +83,7 @@ function displayInputContents(event) {
     let shouldDelete = confirm("Are you sure you want to delete this entry?");
 
     if (shouldDelete) {
-      const lineToDelete = newLine.parentNode;
-      lineToDelete.parentNode.removeChild(lineToDelete);
+      wrapper.parentNode.removeChild(wrapper);
       console.log("deleteFunction");
     }
   }
@@ -97,19 +96,35 @@ function displayInputContents(event) {
       const newDurationH = document.getElementById("duration-h").value;
       const newDurationM = document.getElementById("duration-m").value;
       const newNotes = document.getElementById("notes").value;
-      const newExerciseInput = document.getElementById("exercise-select").value;
-      updatedLine = `${newDate} ${newExerciseInput} ${newDurationH} H ${newDurationM} M ${newNotes}`;
+      const newExerciseInput = getExerciseInputText();
+      const newDateFormat = new Date(newDate);
+      //formatting newly inputted date as DD MM YYYY
+      let day = newDateFormat.getDate();
+      let month = newDateFormat.getMonth() + 1;
+      let year = newDateFormat.getFullYear();
+      let formatOfNewDate = `${day} - ${month} - ${year}`;
+      // capturing the newly inputed results
+      updatedLine = `${formatOfNewDate} ${newExerciseInput} ${newDurationH} H ${newDurationM} M ${newNotes}`;
       console.log(updatedLine);
       editBtn.textContent = "Saved";
-      function savedToEditButton() {
+      // create a function that will reset the button back to it's orignal edit text
+      function btnTextSavedToEdit() {
         editBtn.textContent = "Edit";
       }
-      setTimeout(savedToEditButton, 1000);
+      // set timeout for text in button to go from saved to edit again and reset the input fields
+      setTimeout(btnTextSavedToEdit, 1000);
       document.getElementById("date-select").value = "";
       document.getElementById("duration-h").value = "";
       document.getElementById("duration-m").value = "";
       document.getElementById("notes").value = "";
       document.getElementById("exercise-select").value = "";
+      let spanTextInput = document.createElement("span");
+      spanTextInput.setAttribute("id", "new-span");
+      spanTextInput.textContent = updatedLine;
+
+      wrapper.replaceChild(spanTextInput, currentLine);
+      currentLine = spanTextInput;
+      // if the button still says save (to be saved), we will be caputuring the inputs into variables
     } else {
       editBtn.textContent = "Save";
       document.getElementById("date-select").value = dateInput;
@@ -117,6 +132,10 @@ function displayInputContents(event) {
       document.getElementById("duration-m").value = durationInputM;
       document.getElementById("notes").value = notes;
       document.getElementById("exercise-select").value = exerciseInputValue;
+      console.log(displayAll);
+
+      //   displayAll.replaceChild(spanTextInput, wrapper);
+      // }
     }
   }
   editBtn.addEventListener("click", editInput);
